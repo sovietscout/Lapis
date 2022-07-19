55488517d8fc6dca05d543fe2ee54044f211384e
SovietScout <sovietscout@protonmail.com>
2022-07-19 16:07:08 +0530

    Refactored utils

--- cmd/lapis/generator.go
+++ cmd/lapis/generator.go
@@ -2,9 +2,6 @@ package lapis
 
 import (
 	"image"
-	"image/png"
-	"os"
-	"path/filepath"
 
 	"git.sr.ht/~sbinet/gg"
 )
@@ -20,19 +17,4 @@ type IGenerator interface {
 	Init()
 	Generate() image.Image
 	GetFileName() string
-}
-
-func WriteToFile(filename string, img image.Image) error {
-	outputDir := filepath.Join(".", "output")
-	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-		return err
-	}
-
-	file, err := os.Create(filepath.Join(outputDir, filename))
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	return png.Encode(file, img)
 }
--- cmd/lapis/utils.go
+++ cmd/lapis/utils.go
@@ -2,8 +2,26 @@ package lapis
 
 import (
 	"image"
+	"image/png"
 	"image/color"
+	"os"
+	"path/filepath"
 )
+
+func WriteToFile(filename string, img image.Image) error {
+	outputDir := filepath.Join(".", "output")
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	file, err := os.Create(filepath.Join(outputDir, filename))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return png.Encode(file, img)
+}
 
 func AddMargin(img image.Image, x, y int, colour color.Color) image.Image {
 	// TODO: Center img within margins with given size and colour
